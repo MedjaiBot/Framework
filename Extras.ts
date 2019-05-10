78c9c686302067250d3471dd5209c74e951cf418
Yannick Fricke <yannickfricke@googlemail.com>
2019-05-10 04:07:29 +0200

    refactor(Extras): Added isNullOrUndefined

---
+++ Extras.ts
@@ -0,0 +1,10 @@
+// tslint:disable:no-any
+
+/**
+ * Checks if the given object is null or undefined
+ * @param obj The objecto to check
+ */
+export const IsNullOrUndefined = (obj: any) => {
+    return obj === null ||
+        obj === undefined;
+};
