3457df9cbd2c50b2ca2086f1b72d69f0daec96a8
Yannick Fricke <yannickfricke@googlemail.com>
2019-07-19 21:13:01 +0200

    test(PluginManager): Fixed test

--- src/plugin/__tests__/PluginManager.test.ts
+++ src/plugin/__tests__/PluginManager.test.ts
@@ -4,11 +4,14 @@ import { PluginManager } from '../PluginManager';
 describe('PluginManager', () => {
     let pluginManager: PluginManager;
     let loggerMock: any;
+    let eventManagerMock: any;
 
     beforeEach(() => {
         loggerMock = jest.mock('../../logger/Logger');
+        eventManagerMock = jest.mock('../../event/EventManager');
         pluginManager = new PluginManager(
             loggerMock,
+            eventManagerMock,
         );
     });
 
