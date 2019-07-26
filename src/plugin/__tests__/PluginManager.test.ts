0e38a7179b762b53bc1888c64344d3db2ecd1c0a
Yannick Fricke <yannickfricke@googlemail.com>
2019-07-26 18:08:58 +0200

    test(pluginmanager): fixed the test for the PluginManager

--- src/plugin/__tests__/PluginManager.test.ts
+++ src/plugin/__tests__/PluginManager.test.ts
@@ -5,13 +5,17 @@ describe('PluginManager', () => {
     let pluginManager: PluginManager;
     let loggerMock: any;
     let eventManagerMock: any;
+    let containerMock: any;
 
     beforeEach(() => {
         loggerMock = jest.mock('../../logger/Logger');
         eventManagerMock = jest.mock('../../event/EventManager');
+        containerMock = jest.mock('inversify');
+
         pluginManager = new PluginManager(
             loggerMock,
             eventManagerMock,
+            containerMock,
         );
     });
 
